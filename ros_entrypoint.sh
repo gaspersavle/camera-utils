07c4a23c5f0ea93694bcb0394ab6c3c467a730a5
Gašper Šavle <126724362+gaspersavle@users.noreply.github.com>
2023-08-18 19:53:53 +0200

    Update ros_entrypoint.sh

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-set -e
 
-# setup ros environment
-if [ -z "${SETUP}" ]; then
-    source "/catkin_ws/devel/setup.bash"
-else
-    source $SETUP
-fi
+source "/catkin_ws/devel/setup.bash"
 
 exec "$@"
